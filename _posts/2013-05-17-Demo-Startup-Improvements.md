12f82283718a8a4fa86df4eeb37b5ebc7ea8a7e0
Andre Weissflog <floooh@gmail.com>
2013-05-19 13:02:15 +0200

    Set gzip compression to 'best', and news post

---
+++ _posts/2013-05-17-Demo-Startup-Improvements.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title: Demo Startup Improvements
+tag: news
+---
+
+The latest emscripten incoming branch contains improvements to the internal function table
+which slightly decrease download size (but only a few kByte after gzip in my case), however
+startup time has improved a lot! Check it out on the <a href="demos.html">Demos Page</a>!
--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ mkdir _site/demos
 rsync -a ~/emsctest/cdndata _site/demos
 echo "Gzipping..."
 for file in ~/emsctest/*.html ; do
-	gzip -c $file > _site/demos/$(basename $file).gz
+	gzip --best -c $file > _site/demos/$(basename $file).gz
 done
 for file in ~/emsctest/*.mem ; do
-	gzip -c $file > _site/demos/$(basename $file).gz
+	gzip --best -c $file > _site/demos/$(basename $file).gz
 done
 for file in ~/emsctest/*.js ; do
-	gzip -c $file > _site/demos/$(basename $file).gz
+	gzip --best -c $file > _site/demos/$(basename $file).gz
 done
 echo "Copy htaccess file..."
 cp _htaccess _site/demos/.htaccess
